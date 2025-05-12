6f55a72598e0fc0313e9fd1e32cbe9a3700414c1
LauraGzJones <lauragzjones@gmail.com>
2025-05-12 16:56:21 -0400

    hola

---
+++ hola.js
@@ -0,0 +1,4 @@
+let triple = function (numero) {
+    return numero * 3;}
+    const tripleDec = triple;
+    console.log(triple(10)); // Salida: 30
--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-let frase = "Hola Mundo"
+let frase = "Hola";
 console.log(frase);
 
 // la consola da hola mundo 
-let lista = [agua,leche,azucar]
+let lista = ["agua","leche","azucar"]
 console.log(lista);
 // la consola da agua leche azucar
 
 function suma(a,b) {
     return a + b; 
 }
+let string = "1231563456456"; // Esto debe ser un string, no un array con un solo string
+let longitud = string.length;
+console.log(longitud); // Salida: 13
+// la consola da 13
+
+let string2 ="localizamos un texto dentro de otro texto"
+let posicion = string2.indexOf("Localizamos")
+console.log(posicion); // Salida: 0
+
+// serch()
+//slice ()
+let string3 = "pepe, lolo, juan"
+let resultado = string3.slice(0,4);
+console.log(resultado); // Salida: "pepe"
