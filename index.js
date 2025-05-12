dbc0186721e86766eb8a71fad15ebb90b522eafc
LauraGzJones <lauragzjones@gmail.com>
2025-05-12 10:54:57 -0400

    hola

---
+++ index.js
@@ -0,0 +1,11 @@
+let frase = "Hola Mundo"
+console.log(frase);
+
+// la consola da hola mundo 
+let lista = [agua,leche,azucar]
+console.log(lista);
+// la consola da agua leche azucar
+
+function suma(a,b) {
+    return a + b; 
+}
